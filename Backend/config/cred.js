25a15728e6fc7f073f0172b9392ea09cc0feb0ea
Aryankaushal82 <kaushalaryan1234@gmail.com>
2024-12-13 17:42:55 +0530

    added config

---
+++ Backend/config/cred.js
@@ -0,0 +1,27 @@
+const crd = {
+    user: "aryan1383.be22@chitkara.edu.in",
+    pass: "nayra2210991383"
+  }
+  
+  const moncrd = {
+    user: 'apathania206',
+    pass: 'aryan123'
+  }
+  
+  const twiliocrd = {
+    accountSid: 'AC775f18c361277a19a47c7673bd825a04',
+    authToken: '448336859dd190e24b84962ffec7789a'
+  }
+
+const tokens={
+  ACCESS_TOKEN_SECRET:"Profitex_2425",
+  ACCESS_TOKEN_EXPIRY:"10d",
+  REFRESH_TOKEN_SECRET:"Profitex_2425",
+  REFRESH_TOKEN_EXPIRY:"10d"
+}
+const otp = {
+  user:'aman1249.be22@chitkara.edu.in',
+  pass:'AmanSharma123'
+}
+  
+  module.exports = { crd, moncrd, twiliocrd,tokens,otp};

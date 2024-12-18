1f34275c39a740bef545a1dbd902533273e97336
Aryankaushal82 <kaushalaryan1234@gmail.com>
2024-12-19 03:17:00 +0530

    changes

--- Frontend/src/views/admin/profile/index.jsx
+++ Frontend/src/views/admin/profile/index.jsx
@@ -87,13 +87,13 @@ const ProfileOverview = () => {
 
       <div className="grid h-full grid-cols-1 gap-5 lg:!grid-cols-12">
         <div className="col-span-5 lg:col-span-6 lg:mb-0 3xl:col-span-4">
-          <Project />
+          {/* <Project /> */}
         </div>
         <div className="col-span-5 lg:col-span-6 lg:mb-0 3xl:col-span-5">
-          <General values={value} />
+          {/* <General values={value} /> */}
         </div>
 
-        <div className="col-span-5 lg:col-span-12 lg:mb-0 3xl:!col-span-3">
+        <div className="col-span-5 lg:col-span-12 lg:mb-0 3xl:col-span-12">
           <Notification />
         </div>
       </div>
